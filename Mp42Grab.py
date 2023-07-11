343152b13410dd0d34446f5f9762d7f173ba170b
Tom Lynch <twhlynch@gmail.com>
2023-07-11 12:27:02 +1000

    full V?

---
+++ Mp42Grab.py
@@ -0,0 +1,113 @@
+import cv2, json
+import numpy as np
+from google.protobuf import json_format
+from generated import types_pb2, level_pb2
+
+def createLevel(data, outputFile):
+    level = level_pb2.Level()
+    json_format.Parse(data, level)
+    with open(outputFile, "wb") as f:
+        f.write(level.SerializeToString())
+
+def videoToPixelArray(path):
+    video = cv2.VideoCapture(path)
+    pixel_array = []
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        resized_frame = cv2.resize(frame, (30, 30), interpolation=cv2.INTER_AREA)
+        gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+        normalized_frame = gray_frame.astype(np.float32) / 255.0
+        pixel_array.append(normalized_frame.tolist())
+    video.release()
+    pixel_array = np.array(pixel_array)
+    return pixel_array.tolist()
+
+def pixelsToLevelJSON(pixels):
+    level = {
+        "ambienceSettings": {
+            "skyHorizonColor": {
+                "a": 1.0,
+                "b": 0.9574,
+                "g": 0.9574,
+                "r": 0.916
+            },
+            "skyZenithColor": {
+                "a": 1.0,
+                "b": 0.73,
+                "g": 0.476,
+                "r": 0.28
+            },
+            "sunAltitude": 45.0,
+            "sunAzimuth": 315.0,
+            "sunSize": 1.0
+        },
+        "complexity": 0,
+        "formatVersion": 7,
+        "levelNodes": [],
+        "maxCheckpointCount": 10,
+        "title": "VIDEO BY .INDEX"
+    }
+    for x in range(30):
+        for y in range(30):
+            current = {
+                "levelNodeStatic": {
+                    "color": {
+                        "a": 1.0,
+                        "b": 0.0000024,
+                        "g": 0.0000024,
+                        "r": 0.0000024
+                    },
+                    "material": "DEFAULT_COLORED",
+                    "position": {
+                        "x": x,
+                        "y": y,
+                        "z": 1.0
+                    },
+                    "rotation": {
+                        "w": 1.0
+                    },
+                    "scale": {
+                        "x": 1.0,
+                        "y": 1.0,
+                        "z": 1.0
+                    },
+                    "shape": "SPHERE"
+                },
+                "animations": [
+                    {
+                        "frames": [
+                            {
+                                "position": {},
+                                "rotation": {
+                                    "w": 1.0
+                                }
+                            }
+                        ],
+                        "name": "idle"
+                    }
+                ]
+            }
+            time = 0.01
+            for state in pixels:
+                time += 0.041
+                frame = {
+                    "position": {
+                        "z": state[y][x]
+                    },
+                    "rotation": {
+                        "w": 1.0
+                    },
+                    "time": round(time, 2)
+                }
+                current['animations'][0]['frames'].append(frame)
+            level['levelNodes'].append(current)
+            print(str(x)+','+str(y))
+            print(len(json.dumps(current)))
+
+    return level
+
+pixels_json = videoToPixelArray('video.mp4')
+level_json = pixelsToLevelJSON(pixels_json)
+createLevel(json.dumps(level_json), 'video.level')
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Mp42Grab
+
+### <p style="color: red;">Wont work until animations is implemented into GRAB</p>
+
+- rename your video to `video.mp4`
+- place `video.mp4` and `Mp42Grab.py` into the same folder as the `generated` folder from [Slin/GRAB-Level-Format](https://github.com/Slin/GRAB-Level-Format/tree/main) (main/tools/generated)
+- run `Mp42Grab.py`
